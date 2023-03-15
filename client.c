6164985e7ee5a693309dde5f31ed4055290bcae4
tizhe91015 <peter91015@gmail.com>
2023-03-16 02:23:03 +0800

    Modify client to fit the modified files
    The file `fibdrv.c` is modified and client.c needs modifying to get the
    input of system call and buffer correctly. This is more convenient for
    testing the fibdrv.c

--- client.c
+++ client.c
@@ -11,7 +11,7 @@ int main()
 {
     long long sz;
 
-    char buf[1];
+    char buf[256];
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
@@ -28,22 +28,40 @@ int main()
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, sizeof(buf));
+        // printf("sz = %lld\n", sz);
+        buf[sz] = 0;
+        // printf("fuk my life %s\n",buf);
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%s.\n",
+               i, buf);
     }
 
     for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, sizeof(buf));
+        buf[sz] = '\0';
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%s.\n",
+               i, buf);
     }
+    /*int i=0;
+    xs f[3];
+    int i;
 
+    f[0] = *xs_tmp("0");
+    f[1] = *xs_tmp("1");
+    f[2] = *xs_tmp("1");
+    int tmp;
+    for (i = 2; i <= k; i++) {
+        tmp = i%3;
+        string_number_add(&f[(tmp+1)%3], &f[(tmp+2)%3], &f[tmp]);
+        printf("%s\n", xs_data(&f[tmp]));
+    }
+    tmp = k%3;*/
+    // n = xs_size(&f[tmp]);
     close(fd);
     return 0;
 }
